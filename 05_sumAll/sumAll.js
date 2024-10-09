d06345a73f5249d2a336873e11abceb5a94fc2cf
hak <hak@tutao.de>
2024-10-09 14:16:26 +0200

    add posible solution of 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,17 +1,20 @@
-const sumAll = function (array) {
-  let fullArray = [];
-  array.sort((a, b) => a - b);
-  for (let i = array[0]; i <= array[1]; i++) {
-    fullArray.push(i);
-  }
-  let sum = fullArray.reduce((accumulator, currentValue) => {
-    return accumulator + currentValue;
-  }, 0);
-  return sum;
-};
 
-// Do not edit below this line
-module.exports = sumAll;
+
+// 1st- possible solution
+
+// const sumAll = function (array) {
+//   let fullArray = [];
+//   array.sort((a, b) => a - b);
+//   for (let i = array[0]; i <= array[1]; i++) {
+//     fullArray.push(i);
+//   }
+//   let sum = fullArray.reduce((accumulator, currentValue) => {
+//     return accumulator + currentValue;
+//   }, 0);
+//   return sum;
+// };
+
+
 
 // Description to the function creation:
 
@@ -24,3 +27,18 @@ module.exports = sumAll;
 //     fullArr, run reduce method, creating one single output integer value
 //     return value
 // }
+
+
+
+//2-nd possible solution
+
+const sumAll = function (start, end) {
+let sum = 0;
+for(let i = start; i<=end; i++) {
+    sum+=1;
+}
+return sum;
+};
+
+// Do not edit below this line
+module.exports = sumAll;
