a985bd068ad490ee3139ef698f78fd00e917653a
hak <hak@tutao.de>
2024-10-09 12:52:14 +0200

    solve task 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,26 @@
-const removeFromArray = function() {
+//1-st version of the solution
+
+const  removeFromArray = function (arrays, ...valuesToRemove) {
+   return arrays.filter((number) => !valuesToRemove.includes(number));
 };
+
+
+
+//2-nd version of the solution 
+
+//const removeNumberFromArray = function (array, numbers) {
+  //const newArray = [];
+  //array.forEach((item) => {
+    //if (!numbers.includes(item)) {
+      //newArray.push(item);
+    //}
+  //});
+ // return newArray;
+//};
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+
+
