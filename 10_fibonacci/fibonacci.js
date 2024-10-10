4eb1a74d0418166b73e708c6e33815717ac7851f
hak <hak@tutao.de>
2024-10-10 13:02:10 +0200

    add solution to 10 and 11 tasks

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function (n) {
+  // Base cases
+  if (n == 0) {
+    return 0;
+  } else if (n < 0) {
+    return "OOPS"; // For n = 0
+  } else if (n == 1) {
+    return 1; // For n = 1
+  } else {
+    // Recursive case
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
 };
+//   Example usage:
+//   console.log(fibonacci(5)); // Output: 5 (the 5th Fibonacci number)
 
 // Do not edit below this line
 module.exports = fibonacci;
