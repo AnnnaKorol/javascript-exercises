f548fd96077e02149a19639dec3e7c4132a835da
hak <hak@tutao.de>
2024-10-10 11:04:46 +0200

    solve task 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,45 @@
-const add = function() {
-	
+const add = function (addend1, addend2) {
+  return addend1 + addend2;
 };
 
-const subtract = function() {
-	
+const subtract = function (minuend, subtrahend) {
+  return minuend - subtrahend;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((Summand1, Summand2) => Summand1 + Summand2, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (...values) {
+  return values.reduce((multiplicand, multiplier) => multiplicand * multiplier);
 };
 
-const power = function() {
-	
+const power = function (base, exponent) {
+  return Math.pow(base, exponent);
 };
 
-const factorial = function() {
-	
+const factorial = function (n) {
+  //0! = 1, 1! = 1
+  if (
+    n > 0 &&
+    n <= 1 //or (n === 1)
+  ) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
 };
+
+// 2-nd solution for the factorial :
+// function factorial(n) {
+//   let result = 1;
+//   while (n > 0) {
+//     result *= n;
+//     n -= 1;
+//   }
+//   return result;
+// }
+
 
 // Do not edit below this line
 module.exports = {
@@ -29,5 +48,17 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
+
+// add, subtract, get the sum, multiply, get the power, and find the factorial
+
+// 2-nd solution for the factorial :
+// function factorial(n) {
+//   let result = 1;
+//   while (n > 0) {
+//     result *= n;
+//     n -= 1;
+//   }
+//   return result;
+// }
