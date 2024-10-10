cb60c10d422b790ef8b473844de783a731054ff1
hak <hak@tutao.de>
2024-10-10 13:05:57 +0200

    add comments to task 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -8,3 +8,8 @@ if (year/4 && !year/100 ===0 || !year/1000===0) {
 
 // Do not edit below this line
 module.exports = leapYears;
+
+
+//1) Should be divided by 400 years;
+//2) Should not be divided by 100 year; 
+//3) The leap year is every 4th year;
