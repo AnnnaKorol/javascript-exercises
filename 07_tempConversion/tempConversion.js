436275afb8558e2849e1f6fd23f32c66fd19d3fb
hak <hak@tutao.de>
2024-10-10 10:07:53 +0200

    solve task 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,15 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  return Math.round((fahrenheit - 32) * (5 / 9) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  return Math.round(((celsius * 9) / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
 
-
-//
-//convert number to one character after the decimal point (properly rounded) : Math.round(num * 10) / 10
+//!!!convert number to one character after the decimal point (properly rounded) : Math.round(num * 10) / 10
