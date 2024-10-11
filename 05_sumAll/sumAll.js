758fd08f5e88e163135562a3a40b3a588ce4fc48
hak <hak@tutao.de>
2024-10-11 08:33:58 +0200

    add correction task 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -3,8 +3,8 @@
 // 1st- possible solution
 
 // const sumAll = function (array) {
-//   let fullArray = [];
-//   array.sort((a, b) => a - b);
+/
+//   array.sort((a, b) => a - b);/   let fullArray = [];
 //   for (let i = array[0]; i <= array[1]; i++) {
 //     fullArray.push(i);
 //   }
@@ -32,13 +32,37 @@
 
 //2-nd possible solution
 
-const sumAll = function (start, end) {
-let sum = 0;
-for(let i = start; i<=end; i++) {
-    sum+=1;
-}
-return sum;
-};
+// const sumAll = function (start, end) {
+// let sum = 0;
+// for(let i = start; i<=end; i++) {
+//     sum+=1;
+// }
+// return sum;
+// };
 
-// Do not edit below this line
+
+
+const sumAll = function (min, max) {
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";  //isInteger - static method determines whether the passed value is an integer(целым числом).
+    if (min < 0 || max < 0) return "ERROR";
+    if (min > max) {
+      const temp = min;
+      min = max;
+      max = temp;
+    }
+  
+    // An alternative way to swap the values of min and max like above is to use the array destructuring syntax.
+    // Here's an optional article on it: https://www.freecodecamp.org/news/array-destructuring-in-es6-30e398f21d10/
+    // if (min > max) [min, max] = [max, min];
+    
+    let sum = 0;
+    for (let i = min; i <= max; i++) {
+      sum += i;
+    }
+    return sum;
+  };
+
+
+  // Do not edit below this line
 module.exports = sumAll;
+  
