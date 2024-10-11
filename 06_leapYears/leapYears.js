ba165f7052ac736b304cef852bc26c9b182eafb8
hak <hak@tutao.de>
2024-10-11 08:03:26 +0200

    add solution of the task 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,18 +1,17 @@
-const leapYears = function(year) {
-if (!isNaN(year) && year%4 && year%100 !=== 0 || year
-%1000 !===0) {
-    return true; 
-} else {
+const leapYears = function (year) {
+  if (
+    !isNaN(year) &&
+    ((year % 4 === 0 && year % 100 !== 0 ) || year % 400 === 0 )
+  ) {
+    return true;
+  } else {
     return false;
-}
+  }
 };
 
 // Do not edit below this line
 module.exports = leapYears;
 
-
 //1) Should be divided by 400 years;
-//2) Should not be divided by 100 year; 
+//2) Should not be divided by 100 year;
 //3) The leap year is every 4th year;
-
-
