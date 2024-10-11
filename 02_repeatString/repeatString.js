9f9c41212f58640b8ce2b5bf340a98248c939dc6
hak <hak@tutao.de>
2024-10-11 08:22:28 +0200

    correct task 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,7 +1,11 @@
 const repeatString = function (word, repetitionTime) {
-  if (repetitionTime < 0) return "Error";
+  if (repetitionTime < 0) {
+    return "ERROR";
+  } else if (repetitionTime === 0) {
+    return "";
+  }
   let string = "";
-  for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < repetitionTime; i++) {
     string += word;
   }
   return string;
